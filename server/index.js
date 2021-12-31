36200926d6ee936424008d06606a53c9a00ec0bb
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2021-12-31 16:06:20 +0700

    5. Tạo client sửa 1 chút ở server

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user-routes");
+const postRoutes = require("./routes/post-routes");
+
+const MONGODB_URI = "mongodb://localhost:27017/mern-learn";
+const PORT = 5000;
+
+const app = express();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log("DB CONNECTED ");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(PORT, () => {
+  console.log(`app starting port ${PORT}`);
+});
